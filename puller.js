557326613fcb0f56b2aef304fe22d1800cce5b7c
GrzegorzNecka <strzypior@gmail.com>
2021-06-12 23:10:55 +0200

    2 files modified

--- puller.js
+++ puller.js
@@ -4,6 +4,14 @@ class Puller extends EventEmitter {
   constructor(url) {
     super();
     this.url = url;
+    this.on('removeListener', () => {
+      const number = this.listenerCount('data');
+      //this.listenerCount('data'); - check how many listers are assigned
+
+      if (number === 0) {
+        clearInterval(this.interval);
+      }
+    });
   }
 
   pull() {
@@ -13,6 +21,12 @@ class Puller extends EventEmitter {
         url: this.url
       });
     }, 1000);
+  }
+
+  stop(cb) {
+    //cb mans callback
+    this.removeListener('data', cb);
+    // this event fires up ... this.on('removeListener', () in constructor
   }
 }
 
